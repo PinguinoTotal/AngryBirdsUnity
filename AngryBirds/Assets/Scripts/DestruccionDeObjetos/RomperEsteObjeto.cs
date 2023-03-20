43c3131753547357ebb4a5663631bd72301d3701
Casaga <abraham253253@hotmail.es>
2023-03-20 13:34:54 -0600

    Todas las bases
    Todas las bases con sus archivos en blender se encuentan en el archivo ahora, tambien ya se configuro la gravedad para cada una de ellas

--- AngryBirds/Assets/Scripts/DestruccionDeObjetos/RomperEsteObjeto.cs
+++ AngryBirds/Assets/Scripts/DestruccionDeObjetos/RomperEsteObjeto.cs
@@ -6,11 +6,12 @@ public class RomperEsteObjeto : MonoBehaviour
 {
     [SerializeField] private ElementoEstructuralRompibleSO elementoEstructuralRompibleSO;
     [SerializeField] private Transform contenedorEstructural;
+    private bool objetoRoto = false;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.relativeVelocity.magnitude >= elementoEstructuralRompibleSO.resitencia)
+        if (collision.relativeVelocity.magnitude >= elementoEstructuralRompibleSO.resitencia && !objetoRoto)
         {
-
+            objetoRoto= true;
             //Debug.Log("objeto" + gameObject + "roto");
             if (contenedorEstructural !=null)
             {
